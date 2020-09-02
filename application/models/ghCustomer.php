1db20152bb8c33f4f3b78f5b6212bafb068074fb
qbingking <bqbiamshybee@gmail.com>
2020-09-02 19:35:24 +0700

    update

--- application/models/ghCustomer.php
+++ application/models/ghCustomer.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class GhCustomer extends CI_Model {
+class GhCustomer extends CI_Model { 
     private $table = 'gh_customer';
 
 	public function get($where = []) {
