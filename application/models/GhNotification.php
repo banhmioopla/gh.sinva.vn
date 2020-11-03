cf9fd20103310a89e4a474115f4d4475e2b9579f
qbingking <bqbiamshybee@gmail.com>
2020-11-04 06:36:30 +0700

    update

--- application/models/GhNotification.php
+++ application/models/GhNotification.php
@@ -5,6 +5,7 @@ class GhNotification extends CI_Model {
     private $table = 'gh_notification';
 
 	public function get($where = []) {
+        $this->db->order_by('id DESC');
         return $this->db->get_where($this->table, $where)->result_array();
     }
 
