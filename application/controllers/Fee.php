5cd4f2b6cb642b459fd742cb52d49d46595d86a5
Bánh Mì Ốp La <qbingking@gmail.com>
2020-12-10 19:25:09 +0700

    update

--- application/controllers/Fee.php
+++ application/controllers/Fee.php
@@ -15,14 +15,23 @@ class Fee extends CustomBaseStep {
         $this->custom_execute_general = []; // list Account Id
         $this->load->config('internal_mechanism_income_rate_control_department.php');
         $this->load->config('internal_mechanism_income_rate.php');
+        $this->arr_general = [
+            171020010, // quynh mai
+            171020095, // quocbinh
+            171020036, // pham tiem
+            171020045, // hoang phuong
+            171020053, // thu ngan
+            171020064, // thanh nhan
+            171020047, // bao trinh
+            171020067, // kha ai
+            171020057, // thanh cong
+        ];
 
 
     }
 
 
     public function showOverviewIncome(){
-        $income_cd_config =  $this->config->item('internal_mechanism_income_rate_control_department');
-        $income_sale_config =  $this->config->item('internal_mechanism_income_rate');
         $list_user = $this->ghUser->get([
             'active' => 'YES'
         ]);
@@ -37,11 +46,22 @@ class Fee extends CustomBaseStep {
             $arr_is_control_department[] = $item['code'];
         }
 
+
+        $view_data_income = [];
         foreach ($list_user as $user) {
-            $view_data[$user['account_id']] = $this->syncRoundNumberContractPersonal
+            $view_data_income[$user['account_id']] = $this->syncRoundNumberContractPersonal
             ($user['account_id'],
                 $data['quantity']);
         }
+
+        $this->load->view('components/header',['menu' =>$this->menu]);
+        $this->load->view('fee/show-overview-income', [
+            'list_user_income' => $view_data_income,
+            'libUser' => $this->libUser,
+        ]);
+        $this->load->view('components/footer');
+
+
 
 
 
@@ -85,21 +105,17 @@ class Fee extends CustomBaseStep {
 
     private function syncRoundNumberContractPersonal($user_id = null, $total_contract =
     0, $total_sale = 0) {
-        $cd_config =  $this->config->item('internal_mechanism_income_rate_control_department');
+        $cd_config = $this->config->item('internal_mechanism_income_rate_control_department');
+        $sale_config = $this->config->item('internal_mechanism_income_rate');
         $total_sale_for_cd = $total_sale * (double)
             $cd_config['total_sale_sinva']['basic_control_department'] / 100;
-        $round_contract_to_compare = 0;
-        if($total_contract > 0) {
-            $round_contract_to_compare = $total_contract - ($total_contract%10);
-            // foreach $cd_config
-        }
+
+        $round_contract_to_compare = $total_contract;
 
         $last_date = cal_days_in_month(CAL_GREGORIAN, date('m'), date('Y'));
         $start_date = date('01-m-Y');
         $end_date = date($last_date.'-m-Y');
 
-        $cd_config = $this->config->item('internal_mechanism_income_rate_control_department');
-        $sale_config = $this->config->item('internal_mechanism_income_rate');
 
         $list_contract = $this->ghContract->get([
             'time_check_in >=' => strtotime($start_date),
@@ -112,26 +128,20 @@ class Fee extends CustomBaseStep {
         $user = $this->ghUser->get(['account_id' => $user_id])[0];
         $user_role = $this->ghRole->get(['code' => $user['role_code']])[0];
 
-
         $result['rate'] = 0;
         $is_cd = false;
         $mapping_cd = [];
-        $arr_general = [
-            171020010,
-            171020095,
-            171020036,
-            171020045
-        ];
-        $arr_role_general = ['customer-care', 'humman-resources'];
+        $arr_role_general = ['customer-care', 'human-resources', 'product-manager'];
         if($user_role['is_control_department'] == "YES") {
+            $is_cd = true;
             if(in_array($user_role['code'], $arr_role_general)) {
                 $mapping_cd = $cd_config['index_extra_general'];
             } else {
                 $mapping_cd = $cd_config['index_extra_' . $user['role_code']];
             }
-
         }
-        if(in_array($user['account_id'], $arr_general)) {
+
+        if(in_array($user['account_id'], $this->arr_general)) {
             $is_cd = true;
             $mapping_cd = $cd_config['index_extra_general'];
         }
@@ -139,45 +149,55 @@ class Fee extends CustomBaseStep {
         $total_sale_sinva = 0;
         $max_room_price = 0;
         $max_number_of_month = 0;
+        $max_contract_id = 0;
         $temp1 = 0;
         $total_b1 = 0;
         $total_b2 = 0;
         $total_personal_income = 0;
-
+        $extra_rate_cd = 0;
         if($is_cd) {
             foreach($mapping_cd as $item) {
-                if($item['quantity'] == $round_contract_to_compare) {
-                    $result['income_contract_rate'] = $item['rate'];
-                }
-
-                if($item['quantity'] <= $round_contract_to_compare) {
-                    $result['income_contract_rate'] = $item['rate'];
+                if($item['quantity_min'] <= $round_contract_to_compare && $round_contract_to_compare >= $item['quantity_max']) {
+                    $extra_rate_cd = $item['rate'];
+                    break;
                 }
             }
+
+            $total_extra_personal_income = $extra_rate_cd * (double)$total_sale_for_cd
+                / count($this->arr_general);
 
             foreach ($list_contract as $item) {
                 if($item['room_price']*$item['commission_rate'] > $temp1) {
                     $temp1 = $item['room_price']*$item['commission_rate'];
                     $max_room_price = $item['room_price'];
                     $max_number_of_month = $item['number_of_month'];
+                    $max_contract_id = $item['id'];
                 }
-                foreach ($cd_config['index_master_b2'] as $b2) {
-                    if($item['room_price'] >= $b2['room_price_min'] && $item['room_price'] < $b2['room_price_max']) {
-                        $total_b2 += $b2['income_unit'] * $item['number_of_month'];
-                        break;
-                    }
-                }
-                $total_sale_sinva += $item['room_price'] * $item['commission_rate']/(100);
             }
 
-            foreach ($cd_config['index_master_b2'] as $b1) {
+
+            foreach ($cd_config['index_master_b1'] as $b1) {
                 if($max_room_price >= $b1['room_price_min'] && $max_room_price < $b1['room_price_max']) {
                     $total_b1 += $b1['income_unit'] * $max_number_of_month;
                     break;
                 }
             }
 
-            $total_personal_income = $total_b1 + $total_b2;
+            foreach ($list_contract as $item) {
+                $total_sale_sinva =
+                    $item['commission_rate'] * (double)$item['room_price'] / 100;
+                if($item['id'] === $max_contract_id) {
+                    continue;
+                }
+
+                foreach ($cd_config['index_master_b2'] as $b2) {
+                    if($item['room_price'] >= $b2['room_price_min'] && $item['room_price'] < $b2['room_price_max']) {
+                        $total_b2 += $b2['income_unit'] * $max_number_of_month;
+                    }
+                }
+            }
+
+            $total_personal_income = $total_b1 + $total_b2 + $total_extra_personal_income;
 
         } else {
             $mapping_sale = $sale_config['index_' . $user['role_code']];
@@ -199,11 +219,10 @@ class Fee extends CustomBaseStep {
 
         }
 
-
         return [
             'quantity_contract' => count($list_contract),
             'total_sale' => $total_sale_sinva,
-            'total_personal_income' => $total_personal_income
+            'total_personal_income' => $total_personal_income,
         ];
     }
 
---
+++ application/views/fee/show-overview-income.php
@@ -0,0 +1,76 @@
+<div class="wrapper">
+    <div class="sk-wandering-cubes" style="display:none" id="loader">
+        <div class="sk-cube sk-cube1"></div>
+        <div class="sk-cube sk-cube2"></div>
+    </div>
+    <div class="container-fluid">
+
+        <!-- Page-Title -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="page-title-box">
+                    <div class="btn-group pull-right">
+                        <ol class="breadcrumb hide-phone p-0 m-0">
+                            <li class="breadcrumb-item"><a href="#">test</a></li>
+                            <li class="breadcrumb-item"><a href="#">Extra Pages</a></li>
+                            <li class="breadcrumb-item active">Starter</li>
+                        </ol>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- end page title end breadcrumb -->
+
+        <div class="district-alert"></div>
+        <div class="row">
+            <div class="col-12 col-md-5 offset-md-1 ">
+                <div class="card-box table-responsive shadow">
+                    <h3 class="text-danger text-center">Thống Kê Thu Nhập</h3>
+                    <table id="table-district" class="table">
+                        <thead>
+                        <tr>
+                            <th>Thành Viên</th>
+                            <th class="text-center" width="80px">Số Lượng Hợp Đồng</th>
+                            <th class="text-right">Tổng Doanh Số (x1000)</th>
+                            <th class="text-right">Tổng Thu Nhập(x1000)</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach($list_user_income as $account_id => $item ): ?>
+                            <tr>
+                                <td >
+                                    <div> <?= $libUser->getNameByAccountid($account_id) ?> </div>
+                                </td>
+                                <td class="text-center"><?= number_format($item['quantity_contract']) ?></td>
+
+
+                                <td class="text-right"><?= number_format($item['total_sale']/1000) ?></td>
+                                <td class="text-right"><?= number_format($item['total_personal_income']/1000) ?></td>
+
+                            </tr>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-12 col-md-5">
+                <div class="card-box table-responsive shadow">
+                    <h3 class="text-danger text-center">Thu Nhập Từ Các Khoảng Thưởng</h3>
+                </div>
+            </div>
+        </div> <!-- end row -->
+
+    </div> <!-- end container -->
+</div>
+<!-- end wrapper -->
+
+<script>
+
+    commands.push(function(){
+        $('#table-district').DataTable({
+            "pageLength": 10,
+            'pagingType': "full_numbers",
+            responsive: true,});
+    });
+</script>
