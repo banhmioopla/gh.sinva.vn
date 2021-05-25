020a85f8b6424dc3fe55c301007fd15e046a59ae
Bánh Mì Ốp La <qbingking@gmail.com>
2021-05-25 09:19:08 +0700

    fix bug

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -14,19 +14,24 @@ class Login extends CI_Controller {
 	{
 		$data['account_id'] = $this->input->post('account_id');
 		$data['password'] = $this->input->post('password');
-		if(!empty($_COOKIE['account_id']) AND !empty($_COOKIE['password'])){
-
-			$data['account_id'] = $_COOKIE['account_id'];
-			$data['password'] = $_COOKIE['password'];
+		if(!empty($_COOKIE['gh_account_id']) AND !empty($_COOKIE['gh_account_id'])){
+			$data['account_id'] = $_COOKIE['gh_account_id'];
+			$data['password'] = $_COOKIE['gh_password'];
 			$user_profile = $this->ghUser->login($data);
 
 			if(!empty($user_profile)) {
+
 				$this->session->set_userdata(['auth' => $user_profile]);
                 return redirect($this->default_url);
 			} else {
+                echo "<pre>";
+                var_dump($user_profile); die;
 				return redirect($this->logout_url);
 			}
-		} 
+		} else {
+//		    var_dump("hello");
+//            var_dump($_COOKIE); die;
+        }
 		
 		
 		$submit = $this->input->post('submit');
@@ -35,14 +40,14 @@ class Login extends CI_Controller {
 			if( !empty($user_profile)) {
 				$this->session->set_userdata(['auth' => $user_profile]);
                 $cookie = array(
-                    'name'   => 'account_id',
-                    'value'  => $user_profile['account_id'],
+                    'name'   => 'gh_account_id',
+                    'value'  => "{$user_profile['account_id']}",
                     'expire' => time()+86400*30,
                 );
                 set_cookie($cookie);
                 $cookie = array(
-                    'name'   => 'password',
-                    'value'  => $user_profile['password'],
+                    'name'   => 'gh_password',
+                    'value'  => "{$user_profile['password']}",
                     'expire' => time()+86400*30,
                 );
                 set_cookie($cookie);
@@ -54,11 +59,11 @@ class Login extends CI_Controller {
 	}
 
 	public function logout(){
-		unset($_COOKIE['account_id']);
-		unset($_COOKIE['password']);
+		unset($_COOKIE['gh_account_id']);
+		unset($_COOKIE['gh_password']);
 		unset($_SESSION['auth']);
-		delete_cookie('account_id');
-		delete_cookie('password');
+		delete_cookie('gh_account_id');
+		delete_cookie('gh_password');
 		return $this->show();
 	}
 
