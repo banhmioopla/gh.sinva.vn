11c8ce2a3bfae14c13a07de85e55568834f1423e
Bánh Mì Ốp La <qbingking@gmail.com>
2021-06-21 17:15:16 +0700

    fix bug

--- application/views/apartment/show-version-2.php
+++ application/views/apartment/show-version-2.php
@@ -413,40 +413,6 @@ $check_only_apartment = count($this->list_apartment_view_only) ? true : false;
             });
         });
 
-        // viewmore
-        var showChar = 100;  // How many characters are shown by default
-        var ellipsestext = "...";
-        var moretext = "Xem thêm";
-        var lesstext = "Thu gọn";
-
-        $('.more').each(function() {
-            var content = $(this).html();
-
-            if(content.length > showChar) {
-
-                var c = content.substr(0, showChar);
-                var h = content.substr(showChar, content.length - showChar);
-
-                var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span> <a href="" class="morelink font-600 text-purple">' + moretext + '</a></span>';
-
-                $(this).html(html);
-            }
-
-        });
-
-        $(".morelink").click(function(){
-            if($(this).hasClass("less")) {
-                $(this).removeClass("less");
-                $(this).html(moretext);
-            } else {
-                $(this).addClass("less");
-                $(this).html(lesstext);
-            }
-            $(this).parent().prev().toggle();
-            $(this).prev().toggle();
-            return false;
-        });
-
         $('.add-new-comment').click(function() {
             var apm_id = $(this).data('apm-id');
             var content = $('#apm-comment-'+ apm_id).val();
