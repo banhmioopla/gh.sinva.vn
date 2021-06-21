5cc559f5edf4c54e513f36e26681e9bb2906afeb
Bánh Mì Ốp La <qbingking@gmail.com>
2021-06-21 17:08:20 +0700

    fix bug

--- application/views/apartment/room.php
+++ application/views/apartment/room.php
@@ -6,8 +6,8 @@
             <th># ID</th>
         <?php endif; ?>
             <th>Mã Phòng</th>
-            <th class="text-warning">LP (TN)</th>
-            <th>Giá</th>
+            <th class="text-warning">Loại Phòng</th>
+            <th>Giá <small>x1000</small></th>
             <th>Diện Tích</th>
             <th>Trạng Thái</th>
             <th>Ng.Trống</th>
@@ -48,22 +48,31 @@
                     $list_type_id = json_decode($room['room_type_id'], true);
                     $js_list_type = "";
                     $text_type_name = "";
+
+                    $type_arr = [];
                     if($list_type_id) {
                         $js_list_type = implode(",", $list_type_id);
                         if ($list_type_id && count($list_type_id) > 0) {
                             foreach ($list_type_id as $type_id) {
-                                $typeModel = $ghBaseRoomType->get(['id' => $type_id]);
-                                $text_type_name .= $typeModel[0]['name'] . ', ';
+                                $typeModel = $ghBaseRoomType->getFirstById($type_id);
+                                $type_arr[]= $typeModel['name'];
                             }
                         }
-
                     }
+                    $text_type_name = implode(", ",$type_arr );
+
+                    $status_txt = '<span class="badge badge-danger">Full</span>';
+                    if($room['status'] === 'Available'){
+                        $status_txt = '<span class="badge badge-success">Trống</span>';
+                    }
+
+
                     ?>
                     <td><div><?= $text_type_name ?></div></td>
                     <td><div class="font-weight-bold"><?= number_format($room['price']/1000) ?></div></td>
                     <td><div><?= $room['area'] ?></div></td>
-                    <td class="text-center font-weight-bold <?= $color_for_available ?>"><div><?= $room['status'] ? $label_apartment[$room['status']] : '#' ?></div></td>
-                    <td><div class="text-success"><?= $room['time_available'] ? date('d-m-Y',$room['time_available']) :'' ?></div></td>
+                    <td class="text-center"><div><?= $status_txt ?></div></td>
+                    <td class="text-center"><?= $room['time_available'] ? date('d-m-Y',$room['time_available']) :'' ?></td>
                     <!-- <td><div><?//= $room['consulting_user_id'] ? $libUser->getNameByAccountid($room['consulting_user_id']) :'' ?></div></td> -->
                     <?php if($check_option):?>
                         <td class="d-flex flex-column flex-md-row justify-content-center">
