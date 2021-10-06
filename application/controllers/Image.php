e72e25f6f6496ff34ab4f6f01135b7c417ed7101
Bánh Mì Ốp La <qbingking@gmail.com>
2021-10-06 23:36:24 +0700

    fix bug GH 2 places

--- application/controllers/Image.php
+++ application/controllers/Image.php
@@ -281,7 +281,7 @@ class Image extends CustomBaseStep
     }
 
     public function downloadAllMediaApartment() {
-        ini_set('memory_limit', '22024M');
+        ini_set('memory_limit', '22222024M');
         $this->load->library('zip');
         $rootPath = 'media/apartment/';
         $download_path = 'ImFineThanks';
