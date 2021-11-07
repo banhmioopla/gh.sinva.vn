85fef8df81b8b96a9f5e5e883adbbbf071eb1578
Bánh Mì Ốp La <qbingking@gmail.com>
2021-11-07 20:27:20 +0700

    fix bug GH 2 places

--- application/views/system-income-running/show.php
+++ application/views/system-income-running/show.php
@@ -190,7 +190,7 @@
                                                            href="#collapse-<?= $dd['account_id'] ?>" aria-expanded="false">
                                                             <i class="mdi mdi-account-circle"></i> <?= $dd['name'] ?>
                                                         </a>
-                                                        <span class="pull-right text-primary pl-1"><?= number_format($income_pack['total_sale']) ?></span>
+                                                        <span class="pull-right text-primary pl-1"><?= round($income_pack['total_sale']/$sinva['total_sale'],3)*100 ?>% | <?= number_format($income_pack['total_sale']) ?></span>
                                                     </div>
                                                     <div class="col-12 mt-1">
                                                         <div class="text-primary bg-dark text-center p-1 rounded text-success"><?= number_format($refer_user_income+$income_pack['total_income']) ?></div>
