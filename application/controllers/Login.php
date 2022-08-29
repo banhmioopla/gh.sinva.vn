d86be10323816f384ea825b756d6ffc574f547ca
Bánh Mì Ốp La <qbingking@gmail.com>
2022-08-29 18:26:35 +0700

    fix bug GH 2 places

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -67,7 +67,7 @@ class Login extends CI_Controller {
 		unset($_COOKIE['gh_account_id']);
 		unset($_COOKIE['gh_password']);
 		unset($_SESSION['auth']);
-		unset($_SESSION['income_personal']);
+		unset($_SESSION['total_sale_personal']);
 		unset($_SESSION['isTriggerFiveDay']);
 		delete_cookie('gh_account_id');
 		delete_cookie('gh_password');
