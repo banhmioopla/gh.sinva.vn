df741a3192dbd35476b96cd03681f034d6443758
Bánh Mì Ốp La <qbingking@gmail.com>
2022-08-30 16:14:51 +0700

    fix bug GH 2 places

--- application/controllers/public-world/PublicConsultingPost.php
+++ application/controllers/public-world/PublicConsultingPost.php
@@ -78,7 +78,7 @@ class PublicConsultingPost extends CI_Controller {
                             ]);
                             $count_contract+= count($list_contract);
                             foreach ($list_contract as $con) {
-                                $rate_star += $con['rate_type'];
+                                $rate_star += (float)$con['rate_type'];
                             }
 
                             $list_contract = $this->ghContract->get([
@@ -89,7 +89,7 @@ class PublicConsultingPost extends CI_Controller {
                             ]);
                             $count_contract+= count($list_contract);
                             foreach ($list_contract as $con) {
-                                $rate_star += $con['rate_type'];
+                                $rate_star += (float)$con['rate_type'];
                             }
 
 
