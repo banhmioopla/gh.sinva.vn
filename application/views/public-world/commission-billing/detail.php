c0ff880c35cafa40a586a31b0466aa661dcde4b6
banhmioopla <qbingking@gmail.com>
2023-04-08 17:35:00 +0700

    update web

--- application/views/public-world/commission-billing/detail.php
+++ application/views/public-world/commission-billing/detail.php
@@ -50,58 +50,61 @@
             <div class="col-lg-12">
                 <div class="card-box">
                     <h4 class="font-weight-bold text-danger">Danh sách chi tiết</h4>
-                    <table class="table table-dark">
-                        <thead>
-                        <tr>
-                            <th class="text-center">STT</th>
-                            <th class="text-center">Ngày cọc</th>
-                            <th class="text-center">Mã Phòng</th>
-                            <th class="text-center">Giá Phòng</th>
-                            <th class="text-center">Số tiền phải thanh toán</th>
-                            <th class="text-center">Hoa Hồng</th>
-                            <th class="text-center">Số Tháng</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php
+					<div class="table-responsive">
+						<table class="table  table-dark">
+							<thead>
+							<tr>
+								<th class="text-center">STT</th>
+								<th class="text-center">Ngày cọc</th>
+								<th class="text-center">Mã Phòng</th>
+								<th class="text-center">Giá Phòng</th>
+								<th class="text-center">Số tiền phải thanh toán</th>
+								<th class="text-center">Hoa Hồng</th>
+								<th class="text-center">Số Tháng</th>
+							</tr>
+							</thead>
+							<tbody>
+							<?php
 
-                            $stt = 1;
-                            ?>
+							$stt = 1;
+							?>
 
-                            <?php foreach ($list_contract as $contract):
-                                $room = $this->ghRoom->getFirstById($contract['room_id']);
-                                $contract_total_sale = $this->ghContract->getTotalSaleByContract($contract['id']);
-                                $contract_total_partial = $this->ghContractPartial->getTotalByContractId($contract['id']);
-                                $txt_contract_total_sale = "<span>".number_format($contract_total_sale-$contract_total_partial)."</span>";
-                                $list_partial_arr = []; $list_partial_txt = "";
+							<?php foreach ($list_contract as $contract):
+								$room = $this->ghRoom->getFirstById($contract['room_id']);
+								$contract_total_sale = $this->ghContract->getTotalSaleByContract($contract['id']);
+								$contract_total_partial = $this->ghContractPartial->getTotalByContractId($contract['id']);
+								$txt_contract_total_sale = "<span>".number_format($contract_total_sale-$contract_total_partial)."</span>";
+								$list_partial_arr = []; $list_partial_txt = "";
 
-                                if($contract_total_sale <= $contract_total_partial){
-                                    $txt_contract_total_sale = "<i class='mdi mdi-check-circle text-success'></i> <span class='text-success'>".number_format($contract_total_sale)."</span> <br> <small class='text-success'>thu đủ</small>";
-                                } else {
-                                    $list_partial = $this->ghContractPartial->get(['contract_id' => $contract['id']]);
+								if($contract_total_sale <= $contract_total_partial){
+									$txt_contract_total_sale = "<i class='mdi mdi-check-circle text-success'></i> <span class='text-success'>".number_format($contract_total_sale)."</span> <br> <small class='text-success'>thu đủ</small>";
+								} else {
+									$list_partial = $this->ghContractPartial->get(['contract_id' => $contract['id']]);
 
-                                    foreach ($list_partial as $p){
-                                        $list_partial_arr []= number_format($p['amount']);
-                                    }
-                                    if(count($list_partial_arr)){
-                                        $list_partial_txt = "<div class='text-success'>(".implode(" + ", $list_partial_arr) .")</div>";
-                                    }
+									foreach ($list_partial as $p){
+										$list_partial_arr []= number_format($p['amount']);
+									}
+									if(count($list_partial_arr)){
+										$list_partial_txt = "<div class='text-success'>(".implode(" + ", $list_partial_arr) .")</div>";
+									}
 
-                                }
+								}
 
-                            ?>
-                            <tr  class="text-center">
-                                <th scope="row"><?= $stt ?></th>
-                                <td><?= date("d/m/Y",$contract['time_insert']) ?></td>
-                                <td><?= $room["code"] ?></td>
-                                <td><?= number_format($contract["room_price"]) ?></td>
-                                <td class="text-warning"><?= $txt_contract_total_sale ?></td>
-                                <td><?= $contract["commission_rate"] ?></td>
-                                <td><?= $contract["number_of_month"] ?></td>
-                            </tr>
-                            <?php $stt++; endforeach;?>
-                        </tbody>
-                    </table>
+								?>
+								<tr  class="text-center">
+									<th scope="row"><?= $stt ?></th>
+									<td><?= date("d/m/Y",$contract['time_insert']) ?></td>
+									<td><?= $room["code"] ?></td>
+									<td><?= number_format($contract["room_price"]) ?></td>
+									<td class="text-warning"><?= $txt_contract_total_sale ?></td>
+									<td><?= $contract["commission_rate"] ?></td>
+									<td><?= $contract["number_of_month"] ?></td>
+								</tr>
+								<?php $stt++; endforeach;?>
+							</tbody>
+						</table>
+					</div>
+
                 </div>
 
             </div>
